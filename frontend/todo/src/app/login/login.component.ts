b7330f9fe23883c8388d9a9a17391483988c1b46
Namkhang Le <nnguyen344@gatech.edu>
2023-07-13 09:25:57 -0400

    login working

--- frontend/todo/src/app/login/login.component.ts
+++ frontend/todo/src/app/login/login.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
+
+  username = 'namkhangnle'
+  password = ''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin() {
+    console.log(this.username)
   }
 }
